b9b63277564b108c1cef7cc76f1f18330556a18c
sully34 <sssulai@gmail.com>
2020-03-17 19:39:02 -0400

    initial commit new home nav and error pages

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const About = () => {
+  return(
+    <div>
+      <h1>About Us</h1>
+      <p>About US page body content</p>
+    </div>
+  );
+}
+
+export default About;
